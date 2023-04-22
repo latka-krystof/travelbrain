41d8abeffb4e3169010095e859e601a207b1dfe3
Krystof Latka <krystof.latka@gmail.com>
2023-04-22 15:38:46 -0700

    Integrate search bar with ChatGPT

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,13 +1,32 @@
 import { Link } from "react-router-dom";
+import { useState } from 'react';
 import Chat from '../components/Chat';
+import Map from "../components/Map";
 
 function LandingPage() {
+
+    const [waypoints, setWaypoints] = useState([
+        "Rocco's Tavern",
+        "Poppy",
+        "Muscle Beach"
+    ]);
+
     return (
         <div>
             <h1>TravelBrain.</h1>
             <h2>Your lastminute travel companion.</h2>
             <Link to='/register'>Register here!</Link>
             <Chat/>
+            <Map
+                origin="UCLA, CA"
+                destination="Santa Monica, CA"
+                waypoints={waypoints}
+            />
+            <ul>
+                {waypoints.map((waypoint, index) => (
+                    <li key={index}>{index + 1}. {waypoint}</li>
+                ))}
+            </ul>
         </div>
     )
 }
