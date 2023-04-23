48b1f2c72b616047013122b54bd2ac85b302a6fa
Krystof Latka <krystof.latka@gmail.com>
2023-04-23 03:06:55 -0700

    Add logo

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,16 +1,15 @@
 import { Link } from "react-router-dom";
 import { useState } from 'react';
 
+import Logo from '../assets/Logo.jpg';
+
 function LandingPage() {
 
     return (
-        <div>
-        <h1 className="text-5xl font-bold">TravelBrain</h1>
-        <h2 className="text-4xl font-medium">Your Last-Minute Travel Companion</h2>
-        <div className="mt-8">
-        <Link to='/register' className="py-2 px-4 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">Register now</Link>
+        <div className="flex flex-col h-screen justify-center items-center">
+        <img src={Logo} alt="TravelBrain Logo" className="w-2/3 rounded-lg"/>
+        <Link to='/register' className="py-2 px-4 bg-grey-400 text-black rounded-lg shadow-md  focus:outline-none focus:ring- focus:ring-opacity-75 mb-80 border-2 font-semibold">Register now</Link>
         </div>
-      </div>
     )
 }
 
